f25ba73f2da8fe05ff88ec04f41b4f16e1bba25b
jinpeng.he <jinpeng.he@shopee.com>
2021-09-30 19:37:38 +0800

    add reverse code

--- recursion/reverse_between.go
+++ recursion/reverse_between.go
@@ -6,5 +6,29 @@ type ListNode struct {
 }
 
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
-	
+	// base case
+	if left == 1 {
+		// 相当于反转前 right 个元素
+		reverseN(head, right)
+	}
+	// 前进到反转的起点触发 base case
+	head.Next = reverseBetween(head.Next, left-1, right-1)
+	return head
+}
+
+var successor *ListNode //后驱结点
+
+// 反转以 head 为起点的 n 个结点，返回新的头结点
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		// 记录第 n+1 个结点
+		successor = head.Next
+		return head
+	}
+	// 以 head.Next 为起点，需要反转前 n-1 个结点
+	last := reverseN(head.Next, n-1)
+	head.Next.Next = head
+	// 让反转后的 head 结点和后面的结点连起来
+	head.Next = successor
+	return last
 }
---
+++ recursion/reverse_between_test.go
@@ -0,0 +1,49 @@
+package recursion
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseBetween(t *testing.T) {
+	type args struct {
+		head  *ListNode
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBetween(tt.args.head, tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseBetween() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseN(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseN(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
